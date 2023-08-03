6c1ca95684948f1466cd6d92303347fead98bfcb
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-08-03 13:16:01 +1200

    feat: e

--- src/components/splash/splash.tsx
+++ src/components/splash/splash.tsx
@@ -1,9 +1,19 @@
 import "./splash.css";
 
+import { useNavigate } from "react-router-dom";
+
 const Splash = () => {
+  const navigate = useNavigate();
+
   return (
     <header className="SWW__Splash">
-      <button>SHOP NOW</button>
+      <button
+        onClick={() => {
+          navigate("/catalogue");
+        }}
+      >
+        SHOP NOW
+      </button>
     </header>
   );
 };
