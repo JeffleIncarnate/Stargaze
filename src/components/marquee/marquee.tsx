9aed1d139ffedc7a44860098b5a44c08749fec5e
Dhruv Rayat <dhruvrayat50@gmail.com>
2023-09-03 18:16:44 +1200

    feat: update

--- src/components/marquee/marquee.tsx
+++ src/components/marquee/marquee.tsx
@@ -4,29 +4,37 @@ import Marque from "react-fast-marquee";
 
 const Marquee = () => {
   return (
-    <Marque className="Marquee">
-      <p className="Marquee__Text">
-        WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO SUPPORT@STRGZE.COM
-      </p>
-      <p className="Marquee__Text">
-        WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO SUPPORT@STRGZE.COM
-      </p>
-      <p className="Marquee__Text">
-        WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO SUPPORT@STRGZE.COM
-      </p>
+    <a className="Marquee__Href" href="mailto:support@strgze.com">
+      <Marque className="Marquee">
+        <p className="Marquee__Text">
+          WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO
+          SUPPORT@STRGZE.COM
+        </p>
+        <p className="Marquee__Text">
+          WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO
+          SUPPORT@STRGZE.COM
+        </p>
+        <p className="Marquee__Text">
+          WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO
+          SUPPORT@STRGZE.COM
+        </p>
 
-      <p className="Marquee__Text">
-        WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO SUPPORT@STRGZE.COM
-      </p>
+        <p className="Marquee__Text">
+          WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO
+          SUPPORT@STRGZE.COM
+        </p>
 
-      <p className="Marquee__Text">
-        WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO SUPPORT@STRGZE.COM
-      </p>
+        <p className="Marquee__Text">
+          WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO
+          SUPPORT@STRGZE.COM
+        </p>
 
-      <p className="Marquee__Text">
-        WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO SUPPORT@STRGZE.COM
-      </p>
-    </Marque>
+        <p className="Marquee__Text">
+          WEBSITE STILL UNDER DEVELOPMENT, EMAIL ALL ISSUES TO
+          SUPPORT@STRGZE.COM
+        </p>
+      </Marque>
+    </a>
   );
 };
 
